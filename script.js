86a41287dffe776a7be4c22578298932cfc1ce4e
surajyadav471 <115660802+surajyadav471@users.noreply.github.com>
2023-05-22 14:45:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function threeSum(arr, target) {
 // write your code here
-  
+	let target=[];
+  for(let a = 0;a<arr.length;a++){
+	  target.push(arr[a].toString(2));
+  }
+	return target;
 }
 
 module.exports = threeSum;
